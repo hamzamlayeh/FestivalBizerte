8fe371997f4937f54a516bd073728b73423faa3d
hamzamlayeh <hamzamlayeh@outlook.com>
2019-04-15 20:48:19 +0100

    Login et Inscription

--- app/src/main/java/com/user/festivalbizerte/LoginActivity.java
+++ app/src/main/java/com/user/festivalbizerte/LoginActivity.java
@@ -3,6 +3,7 @@ package com.user.festivalbizerte;
 import android.Manifest;
 import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.content.pm.PackageManager;
 import android.os.Build;
 import android.support.annotation.NonNull;
@@ -10,19 +11,41 @@ import android.support.annotation.RequiresApi;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.View;
+import android.util.Log;
 import android.view.Window;
 import android.view.WindowManager;
+import android.widget.EditText;
+import android.widget.Toast;
 
+import com.google.gson.Gson;
+import com.user.festivalbizerte.Model.RSResponse;
+import com.user.festivalbizerte.Model.User;
+import com.user.festivalbizerte.Model.UserInfos;
+import com.user.festivalbizerte.Utils.Helpers;
+import com.user.festivalbizerte.WebService.WebService;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 import io.github.inflationx.calligraphy3.CalligraphyConfig;
 import io.github.inflationx.calligraphy3.CalligraphyInterceptor;
 import io.github.inflationx.viewpump.ViewPump;
 import io.github.inflationx.viewpump.ViewPumpContextWrapper;
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 
 import static com.user.festivalbizerte.InviteAmisActivity.MY_PERMISSIONS_REQUEST;
 
 public class LoginActivity extends AppCompatActivity {
-
+    Context context;
+    @BindView(R.id.email)
+    EditText Email;
+    @BindView(R.id.password)
+    EditText Password;
+    String email, password;
+    SharedPreferences pref;
+    SharedPreferences.Editor editors;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -30,6 +53,9 @@ public class LoginActivity extends AppCompatActivity {
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
                 WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_login);
+        context = this;
+        ButterKnife.bind(this);
+        pref = getApplicationContext().getSharedPreferences("Users", MODE_PRIVATE);
         ViewPump.init(ViewPump.builder()
                 .addInterceptor(new CalligraphyInterceptor(
                         new CalligraphyConfig.Builder()
@@ -37,21 +63,74 @@ public class LoginActivity extends AppCompatActivity {
                                 .setFontAttrId(R.attr.fontPath)
                                 .build()))
                 .build());
-
-
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
             checkFineLocationPermission();
         }
+
     }
+
     @Override
     protected void attachBaseContext(Context newBase) {
         super.attachBaseContext(ViewPumpContextWrapper.wrap(newBase));
     }
-    public void Inscription(View view) {
-        startActivity(new Intent(this,RegisterActivity.class));
+
+    @OnClick(R.id.txtInscrire)
+    public void InscrirePage() {
+        startActivity(new Intent(context, RegisterActivity.class));
     }
 
-    public void valider(View view) {startActivity(new Intent(this,MainActivity.class));
+    @OnClick(R.id.loginBtn)
+    public void Login() {
+        email = Email.getText().toString().trim();
+        password = Password.getText().toString().trim();
+        if (Valider()) {
+            if (Helpers.isConnected(context)) {
+                User user = new User(email, password);
+                Call<RSResponse> callUpload = WebService.getInstance().getApi().loginUser(user);
+                callUpload.enqueue(new Callback<RSResponse>() {
+                    @Override
+                    public void onResponse(Call<RSResponse> call, Response<RSResponse> response) {
+                        if (response.body() != null) {
+                            if (response.body().getStatus() == 1) {
+                                UserInfos user = new Gson().fromJson(new Gson().toJson(response.body().getData()), UserInfos.class);
+                                editors = pref.edit();
+                                editors.putString("User", new Gson().toJson(user));
+                                editors.apply();
+                                startActivity(new Intent(context, MainActivity.class));
+                            } else if (response.body().getStatus() == 0) {
+                                Toast.makeText(context, "err", Toast.LENGTH_SHORT).show();
+                            } else if (response.body().getStatus() == 2) {
+                                Toast.makeText(context, getString(R.string.EmailOuMotDePasseInvalide), Toast.LENGTH_SHORT).show();
+                            }
+                        }
+                    }
+
+                    @Override
+                    public void onFailure(Call<RSResponse> call, Throwable t) {
+                        Log.i("err", t.getMessage());
+                    }
+                });
+            } else {
+                Helpers.ShowMessageConnection(context);
+            }
+        }
+    }
+
+    private boolean Valider() {
+        boolean valide = true;
+        if (email.isEmpty()) {
+            Email.setError(getString(R.string.champs_obligatoir));
+            valide = false;
+        }
+        if (!email.isEmpty() && (!android.util.Patterns.EMAIL_ADDRESS.matcher(email).matches())) {
+            Email.setError(getString(R.string.email_invalide));
+            valide = false;
+        }
+        if (password.isEmpty()) {
+            Password.setError(getString(R.string.champs_obligatoir));
+            valide = false;
+        }
+        return valide;
     }
 
     @RequiresApi(api = Build.VERSION_CODES.M)
@@ -72,13 +151,11 @@ public class LoginActivity extends AppCompatActivity {
 
     @RequiresApi(api = Build.VERSION_CODES.M)
     public void checkFineLocationPermission() {
-        if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION)
-                != PackageManager.PERMISSION_GRANTED && ContextCompat.checkSelfPermission(this,
+        if (ContextCompat.checkSelfPermission(context, Manifest.permission.ACCESS_COARSE_LOCATION)
+                != PackageManager.PERMISSION_GRANTED && ContextCompat.checkSelfPermission(context,
                 Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-
             requestPermissions(new String[]{Manifest.permission.ACCESS_COARSE_LOCATION,
                     Manifest.permission.ACCESS_COARSE_LOCATION}, MY_PERMISSIONS_REQUEST);
-
         }
     }
 }
--- app/src/main/java/com/user/festivalbizerte/RegisterActivity.java
+++ app/src/main/java/com/user/festivalbizerte/RegisterActivity.java
@@ -1,17 +1,47 @@
 package com.user.festivalbizerte;
 
 import android.content.Context;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Window;
 import android.view.WindowManager;
+import android.widget.EditText;
+import android.widget.Toast;
 
+import com.user.festivalbizerte.Model.RSResponse;
+import com.user.festivalbizerte.Model.UserInfos;
+import com.user.festivalbizerte.Utils.Helpers;
+import com.user.festivalbizerte.WebService.WebService;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 import io.github.inflationx.calligraphy3.CalligraphyConfig;
 import io.github.inflationx.calligraphy3.CalligraphyInterceptor;
 import io.github.inflationx.viewpump.ViewPump;
 import io.github.inflationx.viewpump.ViewPumpContextWrapper;
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 
 public class RegisterActivity extends AppCompatActivity {
+    Context context;
+    @BindView(R.id.regName)
+    EditText Nom;
+    @BindView(R.id.regPrenom)
+    EditText Prenom;
+    @BindView(R.id.regMail)
+    EditText Email;
+    @BindView(R.id.tel)
+    EditText Tel;
+    @BindView(R.id.regPassword)
+    EditText Password;
+    @BindView(R.id.regPassword2)
+    EditText ConfiremPassword;
+    String email, password, nom, prenom, tel, confiremPassword;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -19,20 +49,97 @@ public class RegisterActivity extends AppCompatActivity {
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
                 WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_register);
-
-//        ***init font***
+        context = this;
+        ButterKnife.bind(this);
         ViewPump.init(ViewPump.builder()
                 .addInterceptor(new CalligraphyInterceptor(
                         new CalligraphyConfig.Builder()
                                 .setDefaultFontPath("font/raleway_light.ttf")
                                 .setFontAttrId(R.attr.fontPath)
-                                .build()))
-                .build());
+                                .build())).build());
     }
-    //        ***init font***
+
     @Override
     protected void attachBaseContext(Context newBase) {
         super.attachBaseContext(ViewPumpContextWrapper.wrap(newBase));
     }
 
+    @OnClick(R.id.txtlogin)
+    public void LoginPage() {
+        startActivity(new Intent(context, LoginActivity.class));
+    }
+
+    @OnClick(R.id.regBtn)
+    public void Inscrire() {
+        email = Email.getText().toString().trim();
+        password = Password.getText().toString().trim();
+        nom = Nom.getText().toString().trim();
+        prenom = Prenom.getText().toString().trim();
+        confiremPassword = ConfiremPassword.getText().toString().trim();
+        tel = Tel.getText().toString().trim();
+        if (Valider()) {
+            if (Helpers.isConnected(context)) {
+                UserInfos userInfos = new UserInfos(nom, prenom, email,password, tel);
+                Call<RSResponse> callUpload = WebService.getInstance().getApi().inscrireUser(userInfos);
+                callUpload.enqueue(new Callback<RSResponse>() {
+                    @Override
+                    public void onResponse(Call<RSResponse> call, Response<RSResponse> response) {
+                        if (response.body() != null) {
+                            if (response.body().getStatus() == 1) {
+                                startActivity(new Intent(context, LoginActivity.class));
+                            } else if (response.body().getStatus() == 0) {
+                                Toast.makeText(context, "err", Toast.LENGTH_SHORT).show();
+                            } else if (response.body().getStatus() == 2) {
+                                Toast.makeText(context, getString(R.string.EmailOuMotDePasseInvalide), Toast.LENGTH_SHORT).show();
+                            }
+                        }
+                    }
+
+                    @Override
+                    public void onFailure(Call<RSResponse> call, Throwable t) {
+                        Log.i("err", t.getMessage());
+                    }
+                });
+            } else {
+                Helpers.ShowMessageConnection(context);
+            }
+        }
+    }
+
+    private boolean Valider() {
+        boolean valide = true;
+        if (email.isEmpty()) {
+            Email.setError(getString(R.string.champs_obligatoir));
+            valide = false;
+        }
+        if (!email.isEmpty() && (!android.util.Patterns.EMAIL_ADDRESS.matcher(email).matches())) {
+            Email.setError(getString(R.string.email_invalide));
+            valide = false;
+        }
+        if (nom.isEmpty()) {
+            Nom.setError(getString(R.string.champs_obligatoir));
+            valide = false;
+        }
+        if (prenom.isEmpty()) {
+            Prenom.setError(getString(R.string.champs_obligatoir));
+            valide = false;
+        }
+        if (tel.isEmpty()) {
+            Tel.setError(getString(R.string.champs_obligatoir));
+            valide = false;
+        }
+        if (confiremPassword.isEmpty()) {
+            ConfiremPassword.setError(getString(R.string.champs_obligatoir));
+            valide = false;
+        }
+        if (password.isEmpty()) {
+            Password.setError(getString(R.string.champs_obligatoir));
+            valide = false;
+        }
+        if (!password.isEmpty() && !confiremPassword.isEmpty() && !password.equals(confiremPassword)) {
+            ConfiremPassword.setError("password n ai pas identique ");
+            valide = false;
+        }
+        return valide;
+    }
 }
