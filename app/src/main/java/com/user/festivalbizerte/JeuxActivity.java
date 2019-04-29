060105cb392b9a1f361812cc3ff849aaab6a3278
Hamza Malayeh <hm@steeringit.com>
2019-04-29 12:53:51 +0100

    integration quiz

---
+++ app/src/main/java/com/user/festivalbizerte/JeuxActivity.java
@@ -0,0 +1,13 @@
+package com.user.festivalbizerte;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class JeuxActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_jeux);
+    }
+}
--- app/src/main/java/com/user/festivalbizerte/QuestionsActivity.java
+++ app/src/main/java/com/user/festivalbizerte/QuestionsActivity.java
@@ -6,6 +6,8 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.Window;
 import android.view.WindowManager;
+import android.widget.Button;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.gson.Gson;
@@ -18,7 +20,9 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
+import butterknife.BindView;
 import butterknife.ButterKnife;
+import butterknife.OnClick;
 import io.github.inflationx.calligraphy3.CalligraphyConfig;
 import io.github.inflationx.calligraphy3.CalligraphyInterceptor;
 import io.github.inflationx.viewpump.ViewPump;
@@ -27,8 +31,28 @@ import retrofit2.Callback;
 import retrofit2.Response;
 
 public class QuestionsActivity extends AppCompatActivity {
+
+    @BindView(R.id.nb_question)
+    TextView mNbQuestionView;
+    @BindView(R.id.question)
+    TextView mQuestionView;
+
+    @BindView(R.id.choice1)
+    Button mButtonChoice1;
+    @BindView(R.id.choice2)
+    Button mButtonChoice2;
+    @BindView(R.id.choice3)
+    Button mButtonChoice3;
+
+    @BindView(R.id.rep1Is_valide)
+    TextView mChoice1Valide;
+    @BindView(R.id.rep2Is_valide)
+    TextView mChoice2Valide;
+    @BindView(R.id.rep3Is_valide)
+    TextView mChoice3Valide;
+
     Context context;
-    int Id_Quiz;
+    int Id_Quiz, mScore = 0, mQuestionNumber = 0;
     List<QuestionReponse> listQuestion = new ArrayList<>();
 
     @Override
@@ -48,7 +72,6 @@ public class QuestionsActivity extends AppCompatActivity {
                                 .build()))
                 .build());
         Id_Quiz = getIntent().getExtras().getInt("Id_quiz", 0);
-//        System.out.println(Id_Quiz);
         loadQuestions(Id_Quiz);
 
     }
@@ -63,8 +86,8 @@ public class QuestionsActivity extends AppCompatActivity {
                         if (response.body().getStatus() == 1) {
                             QuestionReponse[] tab = new Gson().fromJson(new Gson().toJson(response.body().getData()), QuestionReponse[].class);
                             listQuestion = Arrays.asList(tab);
-                            Log.i("test",listQuestion.get(0).getListeRep().size()+"/test");
-
+                            //Log.i("test", listQuestion.get(0).getListeRep().size() + "/test");
+                            updateQuestion();
                         } else if (response.body().getStatus() == 0) {
                             Toast.makeText(getApplicationContext(), "Pas de Quiz pour le moument ", Toast.LENGTH_SHORT).show();
                         }
@@ -80,5 +103,49 @@ public class QuestionsActivity extends AppCompatActivity {
         } else {
             Helpers.ShowMessageConnection(context);
         }
+    }
+
+    @OnClick(R.id.choice1)
+    public void ChoiceReponse1() {
+        Toast.makeText(context, mChoice1Valide.getText()+"", Toast.LENGTH_SHORT).show();
+
+    }
+
+    @OnClick(R.id.choice2)
+    public void ChoiceReponse2() {
+        Toast.makeText(context, mChoice1Valide.getText()+"", Toast.LENGTH_SHORT).show();
+    }
+
+    @OnClick(R.id.choice3)
+    public void ChoiceReponse3() {
+        Toast.makeText(context, mChoice1Valide.getText()+"", Toast.LENGTH_SHORT).show();
+    }
+
+    private void updateQuestion() {
+//        Log.i("num", mQuestionNumber + "");
+        if (mQuestionNumber < listQuestion.size()) {
+            mQuestionView.setText(listQuestion.get(mQuestionNumber).getBody());
+            //Reponse1
+            mButtonChoice1.setText(listQuestion.get(mQuestionNumber).getListeRep().get(0).getBody());
+            mChoice1Valide.setText(String.valueOf(listQuestion.get(mQuestionNumber).getListeRep().get(0).getIs_valide()));
+            //Reponse2
+            mButtonChoice2.setText(listQuestion.get(mQuestionNumber).getListeRep().get(1).getBody());
+            mChoice2Valide.setText(String.valueOf(listQuestion.get(mQuestionNumber).getListeRep().get(1).getIs_valide()));
+            //Reponse3
+            mButtonChoice3.setText(listQuestion.get(mQuestionNumber).getListeRep().get(2).getBody());
+            mChoice3Valide.setText(String.valueOf(listQuestion.get(mQuestionNumber).getListeRep().get(2).getIs_valide()));
+
+//            mAnswer = mQuestionLibrary.getCorrectAnswer(mQuestionNumber);
+            mQuestionNumber++;
+            mNbQuestionView.setText(String.format("%s/%s", String.valueOf(mQuestionNumber), String.valueOf(listQuestion.size())));
+
+        } else {
+            mButtonChoice1.setClickable(false);
+            mButtonChoice2.setClickable(false);
+            mButtonChoice3.setClickable(false);
+        }
+    }
+    private void updateScore(int point) {
+        //mScoreView.setText(String.format("%s", String.valueOf(point)));
     }
 }
